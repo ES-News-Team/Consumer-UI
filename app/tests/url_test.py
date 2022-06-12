7da948db4e98600af0c806fa93d319e36841a0ab
Daniel de Souza Lima Junior <39025412+OHomemParede@users.noreply.github.com>
2022-06-12 19:23:27 -0300

    Test 02

--- app/tests/url_test.py
+++ app/tests/url_test.py
@@ -1,10 +1,11 @@
 # import unittest
-# from app.views import url
+from app.views import url
 
 
 # class URLTest(unittest.TestCase):
 #     def test_url(self):
 #         self.asserEqual(url, 'http://127.0.0.1:5002/news/')
         
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    # unittest.main()
+    print(url)
